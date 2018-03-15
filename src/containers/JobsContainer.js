984ff5ae3eb9dfec28f40501e826dd550574facd
Giuseppe <giuseppe.ibba@hotmail.com>
2018-03-15 11:41:41 +0000

    created containers and component folders in src, created JobsCOntainer.js and spiked the API

---
+++ src/containers/JobsContainer.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+
+class JobsContainer extends Component {
+
+  constructor(props) {
+    super(props)
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default JobsContainer;
